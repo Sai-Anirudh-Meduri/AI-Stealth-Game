af54d71ff8083969d656921c6571ae993cc1adb2
Andrew-Soriano <andrew.johnathan.soriano@gmail.com>
2025-12-12 23:14:31 -0500

    Final Content Commit

--- My project/Assets/Scenes/UI/Start UI/MainMenu.cs
+++ My project/Assets/Scenes/UI/Start UI/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("AI Spawner Test");
+        SceneManager.LoadScene("Level");
         
     }
 
